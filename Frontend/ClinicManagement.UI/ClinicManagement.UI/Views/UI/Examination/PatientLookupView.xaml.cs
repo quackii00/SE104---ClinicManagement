44ec0c93377fe02c041fc7252f3431adc99e168a
minhphuong1004 <minhphuong.bunny1004@gmail.com>
2026-06-09 20:59:27 +0700

    style: update UI and views

---
+++ Frontend/ClinicManagement.UI/ClinicManagement.UI/Views/UI/Examination/PatientLookupView.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ClinicManagement.UI.Views.UI.Examination
+{
+    /// <summary>
+    /// Interaction logic for PatientLookupView.xaml
+    /// </summary>
+    public partial class PatientLookupView : UserControl
+    {
+        public PatientLookupView()
+        {
+            InitializeComponent();
+        }
+    }
+}
